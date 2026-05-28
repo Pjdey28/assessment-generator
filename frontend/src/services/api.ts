01565c0060f4d4ce64cb8fdf74f3aa32d1869c0d
Pranjal <sajalkumardey71@gmail.com>
2026-05-29 05:29:48 +0530

    Added base url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
 
-export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-})
+export const api =
+  axios.create({
+    baseURL: process.env.API_URL,
+  })
