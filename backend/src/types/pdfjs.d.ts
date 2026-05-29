daf918a8882069f0a85471ca7943e3d77adf46af
Pranjal <sajalkumardey71@gmail.com>
2026-05-29 14:07:37 +0530

    Fix 11

---
+++ backend/src/types/pdfjs.d.ts
@@ -0,0 +1,11 @@
+declare module "pdfjs-dist/legacy/build/pdf" {
+  const pdfjs: any
+  export default pdfjs
+  export = pdfjs
+}
+
+declare module "pdfjs-dist" {
+  const pdfjs: any
+  export default pdfjs
+  export = pdfjs
+}
